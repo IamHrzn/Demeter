0e90ba786eae14a4f2d2bf15d44a6afe42498420
Limo <99534559+IamHrzn@users.noreply.github.com>
2023-02-12 10:04:00 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IamHrzn/Scourge/main/UI.lua"))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/IamHrzn/Scourge/main/UI.lua"))()
