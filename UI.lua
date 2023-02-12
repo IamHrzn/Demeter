a5d4ec0888456aed87a75554ce6054701b3c117a
Limo <99534559+IamHrzn@users.noreply.github.com>
2023-02-12 10:05:01 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,1 +1,5 @@
 return loadstring(game:HttpGet("https://raw.githubusercontent.com/IamHrzn/Scourge/main/UI.lua"))()
+--[[
+  No point of leaking this. It's not open source + Has some security inbuilt into it. Dont try to hook anything, it will detect most hooking methods to its security 
+  features regardless, anyway. Im hrzn, And i lovr yrur laall!
+]]
